a9187641597461142a98ea414e29eccc907bb435
Tomoki <nowvilla.physi.311@gmail.com>
2022-05-01 14:13:36 +0900

    色の定義を追加した。

--- lib/constant/colors.dart
+++ lib/constant/colors.dart
@@ -1,55 +1,28 @@
 import 'package:flutter/material.dart';
 
-class AppColorCodes {
-  static const _accentColorCode = 0xffbcb0eb;
-  static const _baseColorCode = 0xfff3f3f7;
-  static const _mainColorCode = 0xffb0e5c6;
-  static const _completeColorCode = 0xff2c7afd;
-  static const _editColorCode = 0xff7bc043;
-  static const _deleteColorCode = 0xfffe4a49;
-  static const _boardColorCode = 0xffD4AD7A;
-  static const _saturdayColorCode = 0xffe83316;
-  static const _sundayColorCode = 0xff2168b7;
-  static const _holidayColorCode = _sundayColorCode;
-  static const _blackCode = 0xff1c1c1c;
-  static const _grayCode = 0xffc0c0c0;
-  static const _whiteCode = 0xffffffff;
-  static const _redCode = 0xfff79196;
-}
-
 class AppColors {
-  static const accentColor = Color(AppColorCodes._accentColorCode);
-  static const baseColor = Color(AppColorCodes._baseColorCode);
-  static const mainColor = Color(AppColorCodes._mainColorCode);
-  static const completeColor = Color(AppColorCodes._completeColorCode);
-  static const editColor = Color(AppColorCodes._editColorCode);
-  static const deleteColor = Color(AppColorCodes._deleteColorCode);
-  static const boardColor = Color(AppColorCodes._boardColorCode);
-  static const saturdayColor = Color(AppColorCodes._saturdayColorCode);
-  static const sundayColor = Color(AppColorCodes._sundayColorCode);
-  static const holidayColor = Color(AppColorCodes._holidayColorCode);
-  static const black = Color(AppColorCodes._blackCode);
-  static const gray = Color(AppColorCodes._grayCode);
-  static const white = Color(AppColorCodes._whiteCode);
-  static const red = Color(AppColorCodes._redCode);
-  static const appBarGradientColors = [mainColor, accentColor, red];
+  static const accentColor = Color(0xFF6237CF);
+  static const mainColor = Color(0xFF9ACD32);
+  static const baseColor = Color(0xFFF9F9F8);
+  static const gray = Color(0xFFE5E6E4);
+  static const black = Color(0xFF333333);
 }
 
 class AppMaterialColor {
-  static const _primaryValue = AppColorCodes._mainColorCode;
+  static const _primaryValue = 0xFF9ACD32;
   static const primary = MaterialColor(
     _primaryValue,
     {
-      50: Color(0xFFF6FCF8),
-      100: Color(0xFFE7F7EE),
-      200: Color(0xFFD8F2E3),
-      300: Color(0xFFC8EDD7),
-      400: Color(0xFFBCE9CF),
+      50: Color(0xFFF3F9E6),
+      100: Color(0xFFE1F0C2),
+      200: Color(0xFFCDE699),
+      300: Color(0xFFB8DC70),
+      400: Color(0xFFA9D551),
       500: Color(_primaryValue),
-      600: Color(0xFFA9E2C0),
-      700: Color(0xFFA0DEB9),
-      800: Color(0xFF97DAB1),
-      900: Color(0xFF87D3A4),
+      600: Color(0xFF92C82D),
+      700: Color(0xFF88C126),
+      800: Color(0xFF7EBA1F),
+      900: Color(0xFF6CAE13),
     },
   );
 }
