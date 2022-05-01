764b44b1dfac4531aa9975b9fe973e82a57ab31a
Tomoki <nowvilla.physi.311@gmail.com>
2022-05-01 14:15:34 +0900

    画面を縦に固定した。

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'importer.dart';
 
 void main() {
+  /// 画面を縦に固定する
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(const MyApp());
 }
