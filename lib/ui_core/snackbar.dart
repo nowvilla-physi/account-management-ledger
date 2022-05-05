d1ec4d2981351449c179af4e3886081fa2c34d27
Tomoki <nowvilla.physi.311@gmail.com>
2022-05-06 00:33:07 +0900

    スナックバーの修正

--- lib/ui_core/snackbar.dart
+++ lib/ui_core/snackbar.dart
@@ -23,10 +23,10 @@ class AppSnackbar {
   /// バックアップや復旧に成功した際に表示する
   void showSuccessSnackbar(String text) {
     ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text(Strings.copiedMessage, textAlign: TextAlign.center),
+      SnackBar(
+        content: Text(text, textAlign: TextAlign.center),
         backgroundColor: AppColors.mainColor,
-        duration: Duration(milliseconds: Dimens.snackbarVSuccessDurationMS),
+        duration: const Duration(milliseconds: Dimens.snackbarVSuccessDurationMS),
       ),
     );
   }
